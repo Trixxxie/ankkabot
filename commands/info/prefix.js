b7c1cc67ac29c839daf01b99c1724dd1b36b328a
Pat <patu.tuomineeen@gmail.com>
2020-10-05 21:40:17 +0300

    Fixed global prefix issue

--- commands/info/prefix.js
+++ commands/info/prefix.js
@@ -13,18 +13,20 @@ module.exports = {
         if(!message.member.hasPermission("MANAGE_SERVER")) return message.reply("NO! KWAAK!");
         if(!args[0]) return message.reply("Mihin prefixiin vaihdetaan? Oikea käyttö: `-prefix <uusi prefix>`")
 
-        prefixes[message.guild.id] = {
+        prefixes[message.member.guild.id] = {
             prefixes: args[0]
         };
 
-        fs.writeFile("./data/prefixes.json", JSON.stringify(prefixes), (err) => {
+        fs.writeFile("./prefixes.json", JSON.stringify(prefixes), (err) => {
             if (err) console.log(err)
         });
 
         let prefixEmbed = new Discord.MessageEmbed()
             .setColor("FF9900")
             .setTitle("Prefix vaihdettu!")
-            .setDescription(`Prefix vaihdettu ${args[0]}`);
+            .setDescription(`Prefix vaihdettu ${args[0]}`)
+            .setFooter(`Ankka`, client.user.displayAvatarURL())
+            .setTimestamp();
 
         message.channel.send(prefixEmbed);
     }
