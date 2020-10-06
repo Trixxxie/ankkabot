1786a260a358066c5fdeb457662027ae394c8bb7
Pat <patu.tuomineeen@gmail.com>
2020-10-06 17:30:35 +0300

    Cleaned up randchar.js

--- commands/pathfinder/randchar.js
+++ commands/pathfinder/randchar.js
@@ -7,45 +7,12 @@ module.exports = {
     run: async (client, message, args) => {
         message.delete()
 
-        //SUPER RUMA KOODI HYI HELVETTI -Trixxie
-        
-        let dice1 = Math.floor(Math.random()*(6))
-        let dice2 = Math.floor(Math.random()*(6))
-        let dice3 = Math.floor(Math.random()*(6))
-        let dice4 = Math.floor(Math.random()*(6))
-
-        let dice5 = Math.floor(Math.random()*(6))
-        let dice6 = Math.floor(Math.random()*(6))
-        let dice7 = Math.floor(Math.random()*(6))
-        let dice8 = Math.floor(Math.random()*(6))
-
-        let dice9 = Math.floor(Math.random()*(6))
-        let dice10 = Math.floor(Math.random()*(6))
-        let dice11 = Math.floor(Math.random()*(6))
-        let dice12 = Math.floor(Math.random()*(6))
-
-        let dice13 = Math.floor(Math.random()*(6))
-        let dice14 = Math.floor(Math.random()*(6))
-        let dice15 = Math.floor(Math.random()*(6))
-        let dice16 = Math.floor(Math.random()*(6))
-
-        let dice17 = Math.floor(Math.random()*(6))
-        let dice18 = Math.floor(Math.random()*(6))
-        let dice19 = Math.floor(Math.random()*(6))
-        let dice20 = Math.floor(Math.random()*(6))
-
-        let dice21 = Math.floor(Math.random()*(6))
-        let dice22 = Math.floor(Math.random()*(6))
-        let dice23 = Math.floor(Math.random()*(6))
-        let dice24 = Math.floor(Math.random()*(6))
-
-        var dicegroup1 = [dice1, dice2, dice3, dice4];
-        var dicegroup2 = [dice5, dice6, dice7, dice8];
-        var dicegroup3 = [dice9, dice10, dice11, dice12];
-        var dicegroup4 = [dice13, dice14, dice15, dice16];
-        var dicegroup5 = [dice17, dice18, dice19, dice20];
-        var dicegroup6 = [dice21, dice22, dice23, dice24];
-
+        var dicegroup1 = [Math.floor(Math.random() * 6) + 1, Math.floor(Math.random() * 6) + 1, Math.floor(Math.random() * 6) + 1, Math.floor(Math.random() * 6) + 1];
+        var dicegroup2 = [Math.floor(Math.random() * 6) + 1, Math.floor(Math.random() * 6) + 1, Math.floor(Math.random() * 6) + 1, Math.floor(Math.random() * 6) + 1];
+        var dicegroup3 = [Math.floor(Math.random() * 6) + 1, Math.floor(Math.random() * 6) + 1, Math.floor(Math.random() * 6) + 1, Math.floor(Math.random() * 6) + 1];
+        var dicegroup4 = [Math.floor(Math.random() * 6) + 1, Math.floor(Math.random() * 6) + 1, Math.floor(Math.random() * 6) + 1, Math.floor(Math.random() * 6) + 1];
+        var dicegroup5 = [Math.floor(Math.random() * 6) + 1, Math.floor(Math.random() * 6) + 1, Math.floor(Math.random() * 6) + 1, Math.floor(Math.random() * 6) + 1];
+        var dicegroup6 = [Math.floor(Math.random() * 6) + 1, Math.floor(Math.random() * 6) + 1, Math.floor(Math.random() * 6) + 1, Math.floor(Math.random() * 6) + 1];
 
         const valmis1 = removeSmallest(dicegroup1)
         const valmis2 = removeSmallest(dicegroup2)
