78516b1e02cf58105820f656feebbbc939b10f16
Pat <patu.tuomineeen@gmail.com>
2020-10-06 17:13:32 +0300

    oskuchar.js removed, botinfo.js added

--- commands/info/botinfo.js
+++ commands/info/botinfo.js
@@ -13,6 +13,7 @@ module.exports = {
             .setThumbnail(client.user.displayAvatarURL())
             .setDescription(`Ankka on suomalainen botti SISULLA varustettuna. Toimii apuna kaikissa discord servereissä "työvälineenä", joka helpoittaa moderointia ja tuo hauskoja ominaisuuksia käyttäjille`)
             .addFields(
+                {name: '# Serverillä', value: client.guilds.cache.size, inline: false},
                 {name: 'Kutsulinkki', value: 'https://shorturl.at/iU258', inline: true},
                 {name: 'Lisätietoa', value: 'https://paskakoodi.cam/ankka', inline: true},
                 {name: 'Tuki', value: 'https://discord.gg/S3pyydc'}
